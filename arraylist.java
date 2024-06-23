2f01f60358f113b9ab4881880d5e1831499d803e
parkavivetrivel <bhargavivetrivel27@gmail.com>
2024-06-23 11:42:07 +0530

    arrayList

---
+++ arraylist.java
@@ -0,0 +1,25 @@
+import java.util.*;  
+// import static java.util.Collections.list;
+public class arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+        Scanner s = new Scanner(System.in);
+        int a = 5,i;
+        for(i = 0;i<=a;i++){
+            int b = s.nextInt();
+            list.add(b);            
+        }
+        System.out.println(list);
+        list.add(0,15); //adding in the list specifying the index number.
+        list.get(0); // to retrieve the element in arraylist.
+        list.set(0,13); // updating the element in the array list.
+        list.remove(0); // deleting the element in the arraylist.
+        list.clear(); // deleting all the elemnet in the arraylist.
+        list.size(); // size of the arraylist.
+    }
+    
+}
+
+
+
+
