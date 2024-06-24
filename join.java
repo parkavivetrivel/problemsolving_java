4df064fb350b97e70ac30c2b134092c2fab02069
parkavivetrivel <bhargavivetrivel27@gmail.com>
2024-06-24 12:04:14 +0530

    join in java [] to str

---
+++ join.java
@@ -0,0 +1,7 @@
+public class join {
+    public static void main(String[]arg){
+        String[] a = {"bhar","jan", "dl"};
+        String b = String.join(" ",a);
+        System.out.println(b);
+    }
+}
