3119ccfb42de00c3f768e9bdd637738b93be3099
parkavivetrivel <bhargavivetrivel27@gmail.com>
2024-07-05 12:09:58 +0530

    while loop for linkedlist

---
+++ whileLoop.java
@@ -0,0 +1,14 @@
+public class whileLoop {
+    public static void main(String[] args) {
+        int a = 10;
+        int i = 0;
+        while( i != a){
+            //System.out.println(i);
+            i++;
+        }
+        System.out.println(i);
+        System.out.println(a);
+
+    }
+    
+}
